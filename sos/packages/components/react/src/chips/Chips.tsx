958268fd34df95328f18ef771a4747f186934fcf
celestedayoung <dayoungpyo@gmail.com>
2024-07-28 15:58:42 +0900

    feat: Chips 컴포넌트 및 스토리 추가

---
+++ sos/packages/components/react/src/chips/Chips.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { baseStyle, sizeStyle, stateStyle } from './chips.css';
+
+interface ChipsProps {
+  size?: 's' | 'm' | 'l';
+  variant?: 'primary' | 'secondary';
+  state?: 'default' | 'outline' | 'active' | 'disabled';
+}
+
+export const Chips: React.FC<ChipsProps> = ({
+  size = 's',
+  variant = 'primary',
+  state = 'default',
+}) => {
+  const baseClass = baseStyle;
+  const sizeClass = sizeStyle[size];
+
+  let stateClass = '';
+
+  if (state === 'active') {
+    stateClass =
+      variant === 'primary'
+        ? stateStyle.activePrimary
+        : stateStyle.activeSecondary;
+  } else if (state === 'outline') {
+    stateClass =
+      variant === 'primary'
+        ? stateStyle.outlinePrimary
+        : stateStyle.outlineSecondary;
+  } else {
+    stateClass = stateStyle[state];
+  }
+
+  return <div className={`${baseClass} ${sizeClass} ${stateClass}`}>Label</div>;
+};
