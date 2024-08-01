cfa1c3246426b76fd9c226831a7d436f19fcd272
dayoungpyo <dayoungpyo@gmail.com>
2024-08-01 17:54:55 +0900

    fix: Button 컴포넌트 구조 변경

--- sos/packages/components/react/src/button/Button.tsx
+++ sos/packages/components/react/src/button/Button.tsx
@@ -1,60 +1,69 @@
+import clsx from 'clsx';
+import { forwardRef } from 'react';
 import { CommonProps } from '../common/types';
-import { baseStyle, sizeStyle, stateStyle, mainTextStyle, subTextStyle } from './style.css';
+import { buttonRecipe, ButtonVariants } from './style.css';
+import { Icon } from '../common/icon/Icon';
+import { Text } from '../common/text/Text';
 
-interface ButtonProps extends CommonProps {
-  size: 's' | 'm' | 'l';
-  variant: 'primary' | 'secondary' | 'tertiary';
-  state: 'default' | 'hover' | 'disabled';
-  design: 'fill' | 'outline';
+type ButtonProps = {
   icon?: string;
-  subText?: string;
+  leftSubText?: string;
+  rightSubText?: string;
   mainText?: string;
   isLoading?: boolean;
-}
+  loadingSpinner?: string;
+};
+
+type ButtonExtendedProps = ButtonProps & ButtonVariants & CommonProps;
 
 /**
  * Button 컴포넌트
  *
- * @param {'s' | 'm' | 'l'} props.size - 버튼의 크기 (필수, 기본값: 'm')
- * @param {'primary' | 'secondary' | 'tertiary'} props.variant - 버튼의 변형 스타일 (필수, 기본값: 'primary')
- * @param {'default' | 'hover' | 'disabled'} props.state - 버튼의 상태 (필수, 기본값: 'default')
- * @param {'fill' | 'outline'} props.design - 버튼 디자인 스타일 (필수, 기본값: 'fill')
- * @param {string} [props.icon='add'] - 아이콘 이름 (선택, 기본값: 'add')
- * @param {string} [props.subText='sub'] - 서브 텍스트 (선택, 기본값: 'sub')
- * @param {string} [props.mainText='Main'] - 메인 텍스트 (선택, 기본값: 'Main')
- * @param {boolean} [props.isLoading=true] - 로딩 상태 표시 여부 (선택, 기본값: true)
+ * @param {ButtonVariants['size']} [props.size='m'] - 버튼의 크기 (기본값: 'm')
+ * @param {ButtonVariants['variant']} [props.variant='primary'] - 버튼의 변형 스타일 (기본값: 'primary')
+ * @param {ButtonVariants['state']} [props.state='default'] - 버튼의 상태 (기본값: 'default')
+ * @param {ButtonVariants['design']} [props.design='outline'] - 버튼 디자인 스타일 (기본값: 'outline')
+ * @param {string} [props.icon] - 아이콘 이름 (선택)
+ * @param {string} [props.leftSubText] - 좌측 서브 텍스트 (선택)
+ * @param {string} [props.rightSubText] - 우측 서브 텍스트 (선택)
+ * @param {string} [props.mainText] - 메인 텍스트 (선택)
+ * @param {boolean} [props.isLoading=false] - 로딩 상태 표시 여부 (선택)
+ * @param {string} [props.loadingSpinner='refresh'] - 로딩 스피너 아이콘 (선택, 기본값: 'refresh')
+ * @param {React.Ref<HTMLButtonElement>} ref - 전달받은 ref
  */
-export const Button: React.FC<ButtonProps> = ({
-  size = 'm',
-  variant = 'primary',
-  state = 'default',
-  design = 'fill',
-  icon = 'add',
-  subText = 'sub',
-  mainText = 'Main',
-  isLoading = true,
-}) => {
-  const baseClass = baseStyle;
-  const sizeClass = sizeStyle[size];
-  let stateClass = '';
+export const Button = forwardRef<HTMLButtonElement, ButtonExtendedProps>(
+  (
+    {
+      size = 'm',
+      variant = 'primary',
+      state = 'default',
+      design = 'fill',
+      icon,
+      leftSubText,
+      rightSubText,
+      mainText,
+      isLoading = false,
+      loadingSpinner = 'refresh',
+      className,
+      ...props
+    },
+    ref,
+  ) => {
+    const buttonClass = buttonRecipe({
+      size,
+      variant,
+      state: isLoading ? 'disabled' : state,
+      design,
+    });
 
-  if (variant === 'secondary' || variant === 'tertiary') {
-    stateClass =
-      stateStyle[
-        `${variant}${design.charAt(0).toUpperCase() + design.slice(1)}` as keyof typeof stateStyle
-      ];
-  } else {
-    const stateDesignKey =
-      `${state}${design.charAt(0).toUpperCase() + design.slice(1)}` as keyof typeof stateStyle;
-    stateClass = stateStyle[stateDesignKey];
-  }
-
-  return (
-    <button className={`${baseClass} ${sizeClass} ${stateClass}`}>
-      {icon && <span className="material-symbols-outlined">{icon}</span>}
-      {subText && <span className={subTextStyle}>{subText}</span>}
-      {mainText && <span className={mainTextStyle}>{mainText}</span>}
-      {isLoading && <span className="material-symbols-outlined">refresh</span>}
-    </button>
-  );
-};
+    return (
+      <button ref={ref} className={clsx(buttonClass, className)} {...props} disabled={isLoading}>
+        {icon && <Icon>{icon}</Icon>}
+        {leftSubText && <Text>{leftSubText}</Text>}
+        {mainText && <Text>{mainText}</Text>}
+        {rightSubText && <Text>{rightSubText}</Text>}
+        {isLoading && <Icon>{loadingSpinner}</Icon>}
+      </button>
+    );
+  },
+);
--- sos/packages/components/react/src/button/style.css.ts
+++ sos/packages/components/react/src/button/style.css.ts
@@ -1,116 +1,93 @@
-import { style } from '@vanilla-extract/css';
+import { recipe, RecipeVariants } from '@vanilla-extract/recipes';
 import { vars } from '@sos/style-tokens';
+import { borderSprinkles } from '../../src/style/border/sprinkles.css';
+import { colorSprinkles } from '../../src/style/color/sprinkles.css';
+import { spacingSprinkles } from '../../src/style/spacing/sprinkles.css';
 
-/**
- * 공통 스타일
- */
-export const commonStyle = style({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: '100%',
-  border: 'none',
-  borderRadius: vars.radius.borderRadius.ml,
-  textAlign: 'center',
-  gap: vars.spacing.spacing[2],
+export const buttonRecipe = recipe({
+  base: [
+    {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'center',
+      width: '100%',
+      cursor: 'pointer',
+    },
+    spacingSprinkles({ columnGap: 4 }),
+  ],
+
+  variants: {
+    size: {
+      s: [
+        spacingSprinkles({ paddingY: 2, paddingX: 8 }),
+        { fontSize: vars.typography.fontSize[15], lineHeight: vars.typography.lineHeight[20] },
+      ],
+      m: [
+        spacingSprinkles({ paddingY: 6, paddingX: 12 }),
+        { fontSize: vars.typography.fontSize[16], lineHeight: vars.typography.lineHeight[21] },
+      ],
+      l: [
+        spacingSprinkles({ paddingY: 10, paddingX: 16 }),
+        { fontSize: vars.typography.fontSize[17], lineHeight: vars.typography.lineHeight[22] },
+      ],
+    },
+    variant: {
+      primary: colorSprinkles({ color: 'white', backgroundColor: 'uiPrimaryNormal' }),
+      secondary: colorSprinkles({ color: 'white', backgroundColor: 'blueGray500' }),
+      tertiary: colorSprinkles({ color: 'blueGray500' }),
+    },
+    design: {
+      fill: {},
+      outline: [
+        borderSprinkles({
+          borderWidth: 1,
+          borderStyle: 'solid',
+          borderColor: 'uiPrimaryNormal',
+        }),
+        colorSprinkles({ color: 'uiPrimaryNormal' }),
+      ],
+    },
+    state: {
+      default: {},
+      hover: {},
+      active: {},
+      disabled: colorSprinkles({
+        color: 'blueGray200',
+        backgroundColor: 'blueGray50',
+      }),
+      cursor: 'disabled',
+    },
+  },
+
+  compoundVariants: [
+    {
+      variants: { variant: 'primary', state: 'hover' },
+      style: colorSprinkles({ backgroundColor: 'uiPrimaryHover' }),
+    },
+    {
+      variants: { variant: 'secondary', state: 'hover' },
+      style: colorSprinkles({ backgroundColor: 'blueGray600' }),
+    },
+    {
+      variants: { variant: 'tertiary', state: 'hover' },
+      style: colorSprinkles({ color: 'blueGray600' }),
+    },
+    {
+      variants: { variant: 'primary', design: 'outline', state: 'hover' },
+      style: [
+        borderSprinkles({ borderColor: 'uiPrimaryHover' }),
+        colorSprinkles({ color: 'uiPrimaryHover' }),
+      ],
+    },
+  ],
+
+  defaultVariants: {
+    size: 'm',
+    variant: 'primary',
+    design: 'fill',
+    state: 'default',
+  },
 });
 
-/**
- * Size 스타일
- */
-const sizeVariants = {
-  small: style({
-    padding: `${vars.spacing.spacing[2]} ${vars.spacing.spacing[8]}`,
-    fontSize: vars.typography.fontSize[15],
-    lineHeight: vars.typography.lineHeight[20],
-  }),
-  medium: style({
-    padding: `${vars.spacing.spacing[6]} ${vars.spacing.spacing[12]}`,
-    fontSize: vars.typography.fontSize[16],
-    lineHeight: vars.typography.lineHeight[21],
-  }),
-  large: style({
-    padding: `${vars.spacing.spacing[10]} ${vars.spacing.spacing[16]}`,
-    fontSize: vars.typography.fontSize[17],
-    lineHeight: vars.typography.lineHeight[22],
-  }),
-};
-
-export const sizeStyle = {
-  s: sizeVariants.small,
-  m: sizeVariants.medium,
-  l: sizeVariants.large,
-};
-
-/**
- * State 스타일
- */
-const fillStyles = {
-  default: style({
-    color: vars.color.$static.light.color.white,
-    backgroundColor: vars.color.$palette.ui.primaryNormal,
-  }),
-  hover: style({
-    color: vars.color.$static.light.color.white,
-    backgroundColor: vars.color.$palette.blue[600],
-  }),
-  disabled: style({
-    color: vars.color.$palette.blueGray[200],
-    backgroundColor: vars.color.$palette.blueGray[50],
-  }),
-  secondary: style({
-    color: vars.color.$static.light.color.white,
-    backgroundColor: vars.color.$palette.blueGray[500],
-  }),
-  tertiary: style({
-    color: vars.color.$palette.blueGray[500],
-    backgroundColor: 'transparent',
-  }),
-};
-
-const outlineStyles = {
-  default: style({
-    border: `0.0635rem solid ${vars.color.$palette.ui.primaryNormal}`,
-    color: vars.color.$palette.ui.primaryNormal,
-    backgroundColor: vars.color.$palette.background.normalPrimary,
-  }),
-  hover: style({
-    border: `0.0635rem solid ${vars.color.$palette.blue[700]}`,
-    color: vars.color.$palette.blue[800],
-    backgroundColor: vars.color.$palette.background.normalPrimary,
-  }),
-  disabled: style({
-    border: `0.0635rem solid ${vars.color.$palette.blueGray[50]}`,
-    color: vars.color.$palette.blueGray[200],
-    backgroundColor: vars.color.$palette.background.normalPrimary,
-  }),
-  secondary: style({
-    border: `0.0635rem solid ${vars.color.$palette.gray[200]}`,
-    color: vars.color.$static.light.text.normal,
-  }),
-};
-
-export const stateStyle = {
-  defaultFill: fillStyles.default,
-  defaultOutline: outlineStyles.default,
-  secondaryFill: fillStyles.secondary,
-  secondaryOutline: outlineStyles.secondary,
-  tertiaryFill: fillStyles.tertiary,
-  hoverFill: fillStyles.hover,
-  hoverOutline: outlineStyles.hover,
-  disabledFill: fillStyles.disabled,
-  disabledOutline: outlineStyles.disabled,
-};
-
-/**
- * Text 스타일
- */
-export const mainTextStyle = style({
-  fontWeight: 600,
-});
-
-export const subTextStyle = style({
-  fontWeight: 400,
-});
-
-export const baseStyle = commonStyle;
+export type ButtonVariants = RecipeVariants<typeof buttonRecipe>;
