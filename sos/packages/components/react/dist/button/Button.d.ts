96e77ed62cf0b5db2c963e87da230a5a85804e38
dayoungpyo <dayoungpyo@gmail.com>
2024-08-09 16:41:43 +0900

    feat: Mypage 마크업

---
+++ sos/packages/components/react/dist/button/Button.d.ts
@@ -0,0 +1,16 @@
+import { CommonProps } from '../common/types';
+import { Palette } from '../style/color/sprinkles.css';
+import { ButtonVariants } from './style.css';
+interface ButtonProps extends CommonProps, NonNullable<ButtonVariants> {
+    icon?: string;
+    iconColor?: Palette;
+    leftSubText?: string | number;
+    rightSubText?: string | number;
+    mainText?: string;
+    isLoading?: boolean;
+    loadingSpinner?: string;
+    disabled?: boolean;
+    type?: 'button' | 'submit' | 'reset';
+}
+export declare const Button: import("react").ForwardRefExoticComponent<ButtonProps & import("react").RefAttributes<HTMLButtonElement>>;
+export {};
---
+++ sos/packages/components/react/dist/style/spacing/sprinkles.css.d.ts
@@ -0,0 +1,22 @@
+export declare const spacingSprinkles: ((props: {
+    marginTop?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    marginRight?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    marginBottom?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    marginLeft?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    paddingTop?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    paddingRight?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    paddingBottom?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    paddingLeft?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    gap?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    rowGap?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    columnGap?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    margin?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    padding?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    marginX?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    marginY?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    paddingX?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    paddingY?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+}) => string) & {
+    properties: Set<"columnGap" | "gap" | "margin" | "marginBottom" | "marginLeft" | "marginRight" | "marginTop" | "padding" | "paddingBottom" | "paddingLeft" | "paddingRight" | "paddingTop" | "rowGap" | "marginX" | "marginY" | "paddingX" | "paddingY">;
+};
+export type SpacingSprinkles = Parameters<typeof spacingSprinkles>[0];
