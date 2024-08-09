434ccc325bec47879053ab36bdf4638b4bdbcbf4
dayoungpyo <dayoungpyo@gmail.com>
2024-08-09 16:42:40 +0900

    feat: Mypage 마크업

--- sos/packages/components/react/dist/list/following/FollowingList.d.ts
+++
@@ -1,8 +0,0 @@
-import { CommonProps } from '../../common/types';
-interface FollowingListProps extends CommonProps {
-    nickName?: string;
-    name?: string;
-    onDelete: () => void;
-}
-export declare const FollowingList: import("react").ForwardRefExoticComponent<FollowingListProps & import("react").RefAttributes<HTMLElement>>;
-export {};
--- sos/packages/components/react/dist/tabs/tab/style.css.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const commonStyle: string;
-export declare const stateStyle: {
-    default: string;
-    active: string;
-};
