6ffdb2d919476df0e102f881167019fb30257434
dayoungpyo <dayoungpyo@gmail.com>
2024-08-09 16:59:56 +0900

    feat: My page 마크업

---
+++ sos/packages/components/react/dist/style/border/sprinkles.css.d.ts
@@ -0,0 +1,21 @@
+export declare const borderSprinkles: ((props: {
+    borderColor?: string | number | undefined;
+    borderRadius?: "base" | "none" | "round" | "s" | "xs" | "sm" | "m" | "ml" | "l" | "xl" | "2xl" | undefined;
+    borderStyle?: "none" | "dashed" | "dotted" | "double" | "solid" | undefined;
+    borderWidth?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    borderBottomColor?: string | number | undefined;
+    borderLeftColor?: string | number | undefined;
+    borderRightColor?: string | number | undefined;
+    borderTopColor?: string | number | undefined;
+    borderBottomStyle?: "none" | "dashed" | "dotted" | "double" | "solid" | undefined;
+    borderLeftStyle?: "none" | "dashed" | "dotted" | "double" | "solid" | undefined;
+    borderRightStyle?: "none" | "dashed" | "dotted" | "double" | "solid" | undefined;
+    borderTopStyle?: "none" | "dashed" | "dotted" | "double" | "solid" | undefined;
+    borderBottomWidth?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    borderLeftWidth?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    borderRightWidth?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+    borderTopWidth?: 2 | 1 | 4 | 8 | 16 | 32 | 64 | 6 | 10 | 12 | 14 | 18 | 20 | 24 | 28 | 36 | 40 | 44 | 48 | 56 | undefined;
+}) => string) & {
+    properties: Set<"borderBottomColor" | "borderBottomStyle" | "borderBottomWidth" | "borderColor" | "borderLeftColor" | "borderLeftStyle" | "borderLeftWidth" | "borderRadius" | "borderRightColor" | "borderRightStyle" | "borderRightWidth" | "borderStyle" | "borderTopColor" | "borderTopStyle" | "borderTopWidth" | "borderWidth">;
+};
+export type BorderSprinkles = Parameters<typeof borderSprinkles>[0];
