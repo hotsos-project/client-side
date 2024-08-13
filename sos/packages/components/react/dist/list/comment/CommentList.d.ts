4518748e528e0ac6de783d1235e4193172c18ba2
minukHwang <damin2003@yonsei.ac.kr>
2024-08-13 13:45:50 +0900

    feat: 긴급 신고 페이지 구현

--- sos/packages/components/react/dist/list/comment/CommentList.d.ts
+++
@@ -1,9 +0,0 @@
-import { CommonProps } from '../../common/types';
-interface CommentListProps extends CommonProps {
-    writer: string;
-    date: string;
-    isReply: boolean;
-    isWriter: boolean;
-}
-export declare const CommentList: import("react").ForwardRefExoticComponent<CommentListProps & import("react").RefAttributes<HTMLDivElement>>;
-export {};
--- sos/packages/components/react/dist/loading-spinner/LoadingSpinner.d.ts
+++
@@ -1,2 +0,0 @@
-import { CommonProps } from '../common/types';
-export declare const LoadingSpinner: import("react").ForwardRefExoticComponent<CommonProps & import("react").RefAttributes<HTMLElement>>;
